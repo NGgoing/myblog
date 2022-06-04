a2a32d3f605a5979d7d7c5a3aa6dae2cbd5e543c
ng <111111@qq.com>
2022-06-04 17:21:08 +0800

    confirm: the website's layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Grid } from "@mui/material";
+import LeftPart from "./components/LeftPart";
+import RightPart from "./components/RightPart";
+import "./App.css";
+
 function App() {
-  return <div>App...</div>;
+  return (
+    <Grid className="myapp" container>
+      <Grid className="left" item xs={2} sm={2} md={3} lg={3} xl={3}>
+        <LeftPart></LeftPart>
+      </Grid>
+      <Grid className="right" item xs={9.5} sm={9.5} md={8.5} lg={8.5} xl={8.5}>
+        <RightPart></RightPart>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
---
+++ src/components/RightPart/index.jsx
@@ -0,0 +1,29 @@
+import { Grid, Paper } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+    </Grid>
+  );
+};
